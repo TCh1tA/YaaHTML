57d79090d256d7d544abce1dee2733410d12fd43
TCh1tA <shums-demid@yandex.ru>
2024-03-15 20:09:18 +0300

    апдейт логина

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
+from data.login_form import LoginForm
+from flask_login import LoginManager, login_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect('/succes')
+        else:
+            return render_template('login.html', message='Неверно', form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/succes')
+def suc():
+    return 'Успешно'
+
+# def login_user(user, remember=False):
+#     pass
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 @app.route('/index')
 @app.route('/')
